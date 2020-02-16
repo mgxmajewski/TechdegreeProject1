41e330c5838c26d2a5a6b7f4c9603c58ff289cda
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-02-16 19:01:46 +0100

    Added random background function
    Added random RGB function generator: 1. inserted in the function referance to styling
    2. Used fagment of "document.getElementById('load-quote').addEventListener("click", printQuote, false);"

--- js/script.js
+++ js/script.js
@@ -14,26 +14,26 @@ let quotes = [
   {quote: "Failure is simply the opportunity to begin again, this time more intelligently.",
    source: "Henry Ford",
    citation: "https://www.brainyquote.com/quotes/henry_ford_121339",
-   category: "quote"
+   category: "Quote"
   },
   {quote: "I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
    source: "Albert Einstein",
    citation: "https://www.goodreads.com/author/quotes/9810.Albert_Einstein",
    year: 1929,
-   category: "quote"
+   category: "Quote"
   },
   {quote: "A crisis is an opportunity riding the dangerous wind.",
   source: "Chinese Proverb ",
   citation: "https://www.wow4u.com/chinese/",
-  category: "proverb"
+  category: "Proverb"
  },
  {quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
  source: "Mahatma Gandhi",
- category: "quote"
+ category: "Quote"
 },
 {quote: "“That which does not kill us makes us stronger.”",
 source: "Friedrich Nietzsche",
-category: "quote"
+category: "Quote"
 }
 ];
 console.log(quotes[2]);
@@ -75,19 +75,20 @@ function printQuote () {
 
 console.log(printQuote());
 
-//Creating random color Generator, idea from https://stackoverflow.com/questions/1484506/random-color-generator
+//Creating random color Generator, idea from https://stackoverflow.com/questions/1484506/random-color-generator https://www.youtube.com/watch?v=UzNyAB2Pg7g
+//https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript
 
 function getRandomColor() {
-  var hexString = '0123456789ABCDEF';
-  var RandomColor = '#';
-  for (var i = 0; i < hexString.length; i++) {
-    RandomColor += hexString[Math.floor(Math.random() * hexString.length)];
-  }
-  return RandomColor;
+var r = Math.floor(Math.random()*256);          // Random between 0-255
+var g = Math.floor(Math.random()*256);          // Random between 0-255
+var b = Math.floor(Math.random()*256);          // Random between 0-255
+var rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+return document.body.style.backgroundColor = rgb; // shortest way of direct referal to body coykd find
 }
-console.log(getRandomColor());
-document.getElementById('background-color').style.background = getRandomColor();
 
+let rbgColor = getRandomColor(); //leavin this console command uncomented so it loads initial RGB (otherwise would be white background)
+// console.log(rbgColor);
+addEventListener("click", getRandomColor, false); //Used fagment of "document.getElementById('load-quote').addEventListener("click", printQuote, false);" 
 
 /***
  * click event listener for the print quote button
